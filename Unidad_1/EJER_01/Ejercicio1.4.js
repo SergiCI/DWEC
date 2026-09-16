614ed0692a7ea5426bb31b41bc38ef87c504603f
Sergio <wdv03636@educastur.es>
2026-09-16 10:45:06 +0200

    Ejercicio 1.4

--- Unidad_1/EJER_01/Ejercicio1.4.js
+++ Unidad_1/EJER_01/Ejercicio1.4.js
@@ -4,4 +4,13 @@ let ciudades=["Madrid", "Buenos Aires", "Tokio", "Nueva York", "París"]
 //Añadir otro valor al array con un push
 ciudades.push("Roma")
 
-//
+//Crear el array ciudadesMayusculas
+let ciudadesMayusculas=ciudades.map(ciudad => ciudad.toUpperCase())
+
+//Crear el array ciudadesFiltradas
+let ciudadesFiltradas=ciudades.filter(ciudad => ciudad.length > 6)
+
+//Imprimir los tres arrays en consola
+console.log(ciudades)
+console.log(ciudadesMayusculas)
+console.log(ciudadesFiltradas)
