84fbe95030d9b225e7298d6cf1d09323667548ae
Sergio <wdv03636@educastur.es>
2026-09-23 09:50:39 +0200

    Mitad Ejercicio2.5

--- Unidad_1/EJER_02/Ejercicio2.5.js/gestorUsuarios.js
+++ Unidad_1/EJER_02/Ejercicio2.5.js/gestorUsuarios.js
@@ -10,3 +10,13 @@ export function crearPerfil (nombre,email,edad){
 export function mostrarPerfil(usuario){
     console.log(`Nombre: ${usuario.nombre}, Email: ${usuario.email}, Edad: ${usuario.edad}`)
 }
+// Crea una funcion esMayorDeEdad y usa el export
+export function esMayorDeEdad(usuario) {
+    if (usuario.edad>18) {
+        return true
+    }else{
+        return false
+    }
+}
+
+//Crea la funcion obtenerMayorDeEdad para que reciba un array de usuarios y utilizando el .filter
--- Unidad_1/EJER_02/Ejercicio2.5.js/main.js
+++ Unidad_1/EJER_02/Ejercicio2.5.js/main.js
@@ -1,7 +1,10 @@
 // Importamos las funciones del archivo gestorUsuarios
 import {crearPerfil,mostrarPerfil} from "./gestorUsuarios.js";
 
-const usuarios = [crearPerfil("Sergio","sergio@educastur.es","19"),crearPerfil("Pablo","Pablo@educastur.es","25")]
+const usuarios = [
+    crearPerfil("Sergio","sergio@educastur.es","19"),
+    crearPerfil("Pablo","Pablo@educastur.es","25"
+    )]
 
 usuarios.forEach(usuario => {
     mostrarPerfil(usuario)
